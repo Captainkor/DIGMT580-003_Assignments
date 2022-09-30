f4775d82dc0fb3160eeaaf5de84f8b32a6b33439
Yiyang Shang <shyy451@gmail.com>
2022-09-30 18:50:16 -0400

    initial

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import argparse
-import maya.s
+import maya.standalone
+
+maya.standalone.initialize()
+
+import maya.cmds
+
+print("creating a cube...")
+maya.cmds.polyCube()
+print(maya.cmds.ls(geometry=True))
+
+parser = argparse.ArgumentParser(description = '')
